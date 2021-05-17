b886fdd4bc6da52d99d94baf39564c52fb6a31ed
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2021-05-17 10:12:29 +0700

    initial commit

---
+++ app/src/main/java/com/muabdz/moofie/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.muabdz.moofie.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muabdz.moofie.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}

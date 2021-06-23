4400e75ee5d1e706d604d0508229bb7c4811c2a8
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2021-06-23 19:04:22 +0700

    add tvshows dummy generator

--- app/src/main/java/com/muabdz/moofie/utils/DummyData.kt
+++ app/src/main/java/com/muabdz/moofie/utils/DummyData.kt
@@ -2,6 +2,7 @@ package com.muabdz.moofie.utils
 
 import com.muabdz.moofie.R
 import com.muabdz.moofie.data.MovieEntity
+import com.muabdz.moofie.data.TvShowEntity
 
 object DummyData {
 
@@ -165,5 +166,156 @@ object DummyData {
             ))
 
         return movies
+    }
+
+    fun generateDummyTvShows(): List<TvShowEntity> {
+
+        val tvShows = ArrayList<TvShowEntity>()
+
+        tvShows.add(
+            TvShowEntity(
+                "ts1",
+                "Arrow",
+                "Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow.",
+                "2012",
+                67,
+                "Crime, Drama, Mystery, Action & Adventure",
+                R.drawable.poster_arrow
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts2",
+                "Dragon Ball",
+                "Long ago in the mountains, a fighting master known as Gohan discovered a strange boy whom he named Goku. Gohan raised him and trained Goku in martial arts until he died. The young and very strong boy was on his own, but easily managed. Then one day, Goku met a teenage girl named Bulma, whose search for the mystical Dragon Balls brought her to Goku's home. Together, they set off to find all seven and to grant her wish.",
+                "1986",
+                81,
+                "Animation, Action & Adventure, Sci-Fi & Fantasy",
+                R.drawable.poster_dragon_ball
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts3",
+                "Fairy Tail",
+                "Lucy is a 17-year-old girl, who wants to be a full-fledged mage. One day when visiting Harujion Town, she meets Natsu, a young man who gets sick easily by any type of transportation. But Natsu isn't just any ordinary kid, he's a member of one of the world's most infamous mage guilds: Fairy Tail.",
+                "2009",
+                78,
+                "Action & Adventure, Animation, Comedy, Sci-Fi & Fantasy, Mystery",
+                R.drawable.poster_fairytail
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts4",
+                "Family Guy",
+                "Sick, twisted, politically incorrect and Freakin' Sweet animated series featuring the adventures of the dysfunctional Griffin family. Bumbling Peter and long-suffering Lois have three kids. Stewie (a brilliant but sadistic baby bent on killing his mother and taking over the world), Meg (the oldest, and is the most unpopular girl in town) and Chris (the middle kid, he's not very bright but has a passion for movies). The final member of the family is Brian - a talking dog and much more than a pet, he keeps Stewie in check whilst sipping Martinis and sorting through his own life issues.",
+                "1999",
+                70,
+                "Animation, Comedy",
+                R.drawable.poster_family_guy
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts5",
+                "The Flash",
+                "After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only \"meta-human\" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash.",
+                "2014",
+                77,
+                "Drama, Sci-Fi & Fantasy",
+                R.drawable.poster_flash
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts6",
+                "Game of Thrones",
+                "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+                "2011",
+                84,
+                "Sci-Fi & Fantasy, Drama, Action & Adventure",
+                R.drawable.poster_got
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts7",
+                "Gotham",
+                "Everyone knows the name Commissioner Gordon. He is one of the crime world's greatest foes, a man whose reputation is synonymous with law and order. But what is known of Gordon's story and his rise from rookie detective to Police Commissioner? What did it take to navigate the multiple layers of corruption that secretly ruled Gotham City, the spawning ground of the world's most iconic villains? And what circumstances created them – the larger-than-life personas who would become Catwoman, The Penguin, The Riddler, Two-Face and The Joker?",
+                "2014",
+                75,
+                "Drama, Crime, Sci-Fi & Fantasy",
+                R.drawable.poster_gotham
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts8",
+                "Grey's Anatomy",
+                "Follows the personal and professional lives of a group of doctors at Seattle’s Grey Sloan Memorial Hospital.",
+                "2005",
+                82,
+                "Drama",
+                R.drawable.poster_grey_anatomy
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts9",
+                "Marvel's Iron Fist",
+                "Danny Rand resurfaces 15 years after being presumed dead. Now, with the power of the Iron Fist, he seeks to reclaim his past and fulfill his destiny.",
+                "2017",
+                66,
+                "Action & Adventure, Drama, Sci-Fi & Fantasy",
+                R.drawable.poster_iron_fist
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts10",
+                "Naruto Shippūden",
+                "Naruto Shippuuden is the continuation of the original animated TV series Naruto.The story revolves around an older and slightly more matured Uzumaki Naruto and his quest to save his friend Uchiha Sasuke from the grips of the snake-like Shinobi, Orochimaru. After 2 and a half years Naruto finally returns to his village of Konoha, and sets about putting his ambitions to work, though it will not be easy, as He has amassed a few (more dangerous) enemies, in the likes of the shinobi organization; Akatsuki.",
+                "2007",
+                86,
+                "Animation, Action & Adventure, Sci-Fi & Fantasy",
+                R.drawable.poster_naruto_shipudden
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts11",
+                "NCIS",
+                "From murder and espionage to terrorism and stolen submarines, a team of special agents investigates any crime that has a shred of evidence connected to Navy and Marine Corps personnel, regardless of rank or position.",
+                "2003",
+                75,
+                "Crime, Action & Adventure, Drama",
+                R.drawable.poster_ncis
+            )
+        )
+
+        tvShows.add(
+            TvShowEntity(
+                "ts12",
+                "The Simpsons",
+                "Set in Springfield, the average American town, the show focuses on the antics and everyday adventures of the Simpson family; Homer, Marge, Bart, Lisa and Maggie, as well as a virtual cast of thousands. Since the beginning, the series has been a pop culture icon, attracting hundreds of celebrities to guest star. The show has also made name for itself in its fearless satirical take on politics, media and American life in general.",
+                "1989",
+                79,
+                "Family, Animation, Comedy, Drama",
+                R.drawable.poster_the_simpson
+            )
+        )
+
+        return tvShows
     }
 }
